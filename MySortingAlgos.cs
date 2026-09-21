36c37d777515f1083bc9e7e618fa412de3bbf89b
RockHoang <peter.huy@hotmail.com>
2026-09-21 08:46:58 +0200

    Add project files.

---
+++ MySortingAlgos.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace SortingAlgorithm
+{
+    public static class MySortingAlgos
+    {
+      
+
+        
+        public static ArrayList InsertionSort(ArrayList a)
+        {
+            Stopwatch sw = new Stopwatch();
+            Console.WriteLine("Insertion Sort");
+            
+            sw.Start();
+            for (int i = 1;i < a.Count; i++)
+            {
+                int key = (int)a[i];
+                int j = i - 1;
+                while (j >= 0 && (int)a[j] > key)
+                {
+                    a[j + 1] = a[j];
+                    j--;
+                }
+                a[j + 1] = key;
+                //Console.WriteLine(i);
+            }
+            sw.Stop();
+            TimeSpan ts = sw.Elapsed;
+            Console.WriteLine($"Sorting took {ts}.");
+            return a;
+        }
+
+        public static ArrayList SelectionSort(ArrayList a)
+        {
+            Stopwatch sw = new Stopwatch();
+            Console.WriteLine("Selection Sort");
+            sw.Start();
+            for (int i = 0; i < a.Count-1; ++i) {
+                int min = i;
+                for (int j = i+1; j < a.Count; ++j) {
+                    if ((int)a[j] < (int)a[min])
+                    {
+                        min = j;
+                    }
+                }
+                if (min != i)
+                {
+                    int temp = (int)a[i];
+                    a[i] = a[min];
+                    a[min] = temp;
+                }
+            }
+            sw.Stop();
+            TimeSpan ts = sw.Elapsed;
+            Console.WriteLine($"Sorting took {ts}.");
+            return a;
+        }
+        private static ArrayList merge(ArrayList left, ArrayList right)
+        {
+            ArrayList result = new ArrayList();
+            while (left.Count > 0 && right.Count > 0)
+            {
+                if ((int)left[0] <= (int)right[0] )
+                {
+                    result.Add(left[0]);
+                    left.RemoveAt(0);
+                }
+                else
+                {
+                    result.Add(right[0]);
+                    right.RemoveAt(0);
+                }
+            }
+
+            while (left.Count > 0) {
+                result.Add(left[0]);
+                left.RemoveAt(0);
+            }
+
+            while (right.Count > 0)
+            {
+                result.Add(right[0]);
+                right.RemoveAt(0);
+            }
+
+            return result;
+        }
+
+        private static ArrayList MergeSort(ArrayList m)
+        {
+            if (m.Count <= 1)
+            {
+                return m;
+            }
+
+            ArrayList leftList = new ArrayList();
+            ArrayList rightList = new ArrayList();
+            for (int i = 0; i < m.Count; i++)
+            {
+                if(i < (m.Count / 2))
+                {
+                    leftList.Add(m[i]);
+                }
+                else
+                {
+                    rightList.Add(m[i]);
+                }
+            }
+
+            return merge(leftList, rightList);
+        }
+
+        public static ArrayList MergeSortcall(ArrayList m)
+         {
+            Console.WriteLine("Merge Sort");
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            ArrayList sortedlist = new ArrayList(MergeSort(m));
+            sw.Stop();
+            TimeSpan ts = sw.Elapsed;
+            Console.WriteLine($"Sorting took {ts}.");
+            return sortedlist;
+         }
+}
+}
---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using SortingAlgorithm;
+using System.Collections;
+
+Console.WriteLine("This program sorts some numbers.");
+Random randomInt = new Random();
+ArrayList myCollection = new ArrayList();
+Console.WriteLine("how many numbers do you want?");
+int amountofInts = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < amountofInts; i++)
+{
+    int myint = randomInt.Next(1, 100);
+    myCollection.Add(myint);
+}
+
+foreach (int item in myCollection)
+{
+    Console.Write($"{item} ");
+}
+Console.WriteLine();
+Console.WriteLine("Start sorting!");
+
+foreach (int item in MySortingAlgos.InsertionSort(myCollection))
+{
+    Console.Write($"{item} ");
+}
+Console.WriteLine();
+foreach (int item in MySortingAlgos.SelectionSort(myCollection))
+{
+    Console.Write($"{item} ");
+}
+
+Console.WriteLine();
+foreach (int item in MySortingAlgos.MergeSortcall(myCollection))
+{
+    Console.Write($"{item} ");
+}
